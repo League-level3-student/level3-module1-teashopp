6dced306dbc45fa7abc00294d407266a381bd96f
ethan <ethan.yuan@gmail.com>
2021-12-12 14:25:40 -0800

    12 / 12

--- src/_00_Intro_To_ArrayLists/_02_GuestBook.java
+++ src/_00_Intro_To_ArrayLists/_02_GuestBook.java
@@ -1,10 +1,16 @@
 package _00_Intro_To_ArrayLists;
 
+import java.awt.Component;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
-public class _02_GuestBook {
+public class _02_GuestBook implements ActionListener {
 	/*
 	 * Create a GUI with two buttons. One button reads "Add Name" and the other
 	 * button reads "View Names". When the add name button is clicked, display an
@@ -15,22 +21,50 @@ public class _02_GuestBook {
 	 * #4: Donny Doners
 	 */
 
+	// framework
 	JFrame frame = new JFrame();
 	JPanel panel = new JPanel();
-	static JButton addbutton = new JButton("Add Name");
-	static JButton viewbutton = new JButton("View Names");
+	JButton addbutton = new JButton("Add Name");
+	JButton viewbutton = new JButton("View Names");
+    ArrayList<String> gbook = new ArrayList<String>();
 
+	// main
+	public static void main(String[] args) {
+
+		_02_GuestBook gb = new _02_GuestBook();
+		gb.setup();
+
+	}
+
+	// set up
 	private void setup() {
 		frame.add(panel);
 		panel.add(addbutton);
 		panel.add(viewbutton);
-	}
-	
-	
-	
-	public static void main(String[] args) {
-		
-		
+
+		frame.setVisible(true);
+		panel.setVisible(true);
+		addbutton.setVisible(true);
+		viewbutton.setVisible(true);
+
+		frame.pack();
+
+		addbutton.addActionListener(this);
+		viewbutton.addActionListener(this);
 	}
 
+	// button pressed
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		if (addbutton == e.getSource()) {
+			gbook.add(JOptionPane.showInputDialog("Please enter a name into the Guestbook:"));
+		}
+		
+		else if (viewbutton == e.getSource()) {
+			for (int i = 0; i < gbook.size(); i++) {
+				System.out.println("Guest #" + (i+1) + ": " + gbook.get(i));
+			}
+		}
+	}
 }
